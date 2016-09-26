86f40b5bc1ff9a742606cb1efd9f2a36e67e1362
Christopher Allan <nerd3d@hotmail.com>
2016-09-26 15:24:39 -0600

    Adding Folders for Organization

---
+++ 3710_Platformer/Assets/PreFabs/README_prefabs.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class README_prefabs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
