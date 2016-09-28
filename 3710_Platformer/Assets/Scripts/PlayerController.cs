9c93e945c1c217dd820c80941503eac47e953f01
Christopher Allan <nerd3d@hotmail.com>
2016-09-28 08:42:30 -0600

    Modified Movement Friction and Commented Player Script

--- 3710_Platformer/Assets/Scripts/PlayerController.cs
+++ 3710_Platformer/Assets/Scripts/PlayerController.cs
@@ -3,44 +3,59 @@ using System.Collections;
 using Prime31;
 using UnityEngine.UI;
 
+/// <summary>
+/// This class is meant to control the player character.
+/// It assumes the game is a single player game and may not work
+///     with multiple players.
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
 
-    private CharacterController2D _controller;
-    private AnimationController2D _animator;
-    private float currentHealth = 0;
-    private bool PlayerControl = true;
+    private CharacterController2D _controller;  // Prime31 2D controller
+    private AnimationController2D _animator;    // Animation Controller
+    private float currentHealth;                // Protected Player Health
+    private bool PlayerControl = true;          // Player input toggle
 
     public GameObject gameOverPanel;
     public GameObject gameCamera;
     public GameObject healthBar;
-    public float gravity = -35;
-    public float walkSpeed = 3;
-    public float jumpHeight = 2;
-    public int health = 100;
+    public float gravity = -35;                 // gravity for player
+    public float walkSpeed = 3;                 // player walk speed
+    public float slideFriction = 0.2f;          // sliding friction for player
+    public float jumpHeight = 2;                // Jump Height
+    public int health = 100;                    // Player Starting Health
 
-    // Use this for initialization
+    /// <summary>
+    /// Class Start Function. Initializes Controllers and starts the CameraFollow
+    /// </summary>
     void Start()
     {
         _controller = gameObject.GetComponent<CharacterController2D>();
         _animator = gameObject.GetComponent<AnimationController2D>();
         gameCamera.GetComponent<CameraFollow2D>().startCameraFollow(this.gameObject);
 
-        currentHealth = health;
+        currentHealth = health; // set starting health
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Frame Update method. Called every frame
+    /// </summary>
     void Update()
     {
 
         if (PlayerControl)
-            _controller.move(PlayerInput() * Time.deltaTime);
+            _controller.move(PlayerInput() * Time.deltaTime); // player movement references PlayerInput
     }
 
+    /// <summary>
+    /// Looks at player input and determines a movement vector
+    /// </summary>
+    /// <returns>Movement Vector (velocity)</returns>
     private Vector3 PlayerInput()
     {
         Vector3 velocity = _controller.velocity; // initial velocity
 
+        // If player lands on a moving platform, child the player to that platform, so he'll move with it
         if (_controller.isGrounded && _controller.ground != null && _controller.ground.tag == "MovingPlatform")
         {
             this.transform.parent = _controller.ground.transform;
@@ -51,6 +66,7 @@ public class PlayerController : MonoBehaviour
                 transform.parent = null;
         }
 
+        // if horizontal input is negative, move left
         if (Input.GetAxis("Horizontal") < 0)
         { // Move Left
             velocity.x = walkSpeed * -1;
@@ -58,6 +74,7 @@ public class PlayerController : MonoBehaviour
                 _animator.setAnimation("Run");
             _animator.setFacing("Left");
         }
+        // if horizontal input is positive, move right
         else if (Input.GetAxis("Horizontal") > 0)
         { // Move Right
             velocity.x = walkSpeed;
@@ -65,28 +82,34 @@ public class PlayerController : MonoBehaviour
                 _animator.setAnimation("Run");
             _animator.setFacing("Right");
         }
+        // else, player is idle
         else
         {
             if (_controller.isGrounded)
                 _animator.setAnimation("Idle");
         }
 
+        // if jump is pressed & player is grounded, player jumps
         if (Input.GetAxis("Jump") > 0 && _controller.isGrounded)
         {
-            velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
+            velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity); // jump height is a scalar manupulation of gravity
             _animator.setAnimation("Jump");
         }
 
-        velocity.x *= 0.8f; // light friction
+        velocity.x *= (1-slideFriction); // apply friction
 
-        velocity.y += gravity * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime; // apply gravity
 
-        return velocity;
+        return velocity; // return the new velicity vector
     }
 
+    /// <summary>
+    /// Trigger boxes with a "sustained" effect will use this function
+    /// </summary>
+    /// <param name="col">Collider with effect</param>
     void OnTriggerStay2D(Collider2D col)
     {
-        if (col.tag == "Damaging")
+        if (col.tag == "Damaging") // Damaging effect
         {
             PlayerDamage(20 * Time.deltaTime);
 
@@ -94,39 +117,53 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Trigger boxes with an "enter" effect will use this function
+    /// </summary>
+    /// <param name="col">Collider with effect</param>
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.tag == "KillZ")
+        if (col.tag == "KillZ") // insta-kill effect
         {
             PlayerFallDeath();
         }
-        else if (col.tag == "Damaging")
+        else if (col.tag == "Damaging") // damage effect
         {
             PlayerDamage(20);
         }
     }
 
+    /// <summary>
+    /// Handles Player damage recieved
+    /// </summary>
+    /// <param name="damage">Ammount of damage recived</param>
     private void PlayerDamage(float damage)
     {
         currentHealth -= damage;
         float normalizedHealth = (float)currentHealth / (float)health;
         GameObject.Find("Health").GetComponent<RectTransform>().sizeDelta = new Vector2(normalizedHealth * 256, 32);
-        if (currentHealth <= 0)
+        if (currentHealth <= 0) // is player dead
             PlayerDeath();
     }
 
+    /// <summary>
+    /// Handles Player Death
+    /// </summary>
     private void PlayerDeath()
     {
         _animator.setAnimation("PlayerDeath");
         PlayerControl = false;
-        //gameOverPanel.SetActive(true);
+        //gameOverPanel.SetActive(true); // Disabled until GameOver Panel is implemented
     }
 
+    /// <summary>
+    /// Handles Pleayer death due to falling (includes camera Stop Follow)
+    /// </summary>
     private void PlayerFallDeath()
     {
         currentHealth = 0;
         GameObject.Find("Health").GetComponent<RectTransform>().sizeDelta = new Vector2(0, 32);
         gameCamera.GetComponent<CameraFollow2D>().stopCameraFollow();
-        //gameOverPanel.SetActive(true);
+        //gameOverPanel.SetActive(true); // Disabled until GameOver Panel is implemented
     }
 }
