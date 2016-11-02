701633bc03fca246656011436f02f03f57b9bb1e
Christopher Allan <nerd3d@hotmail.com>
2016-11-02 09:42:06 -0600

    Added Scroll Animation and Prefab

--- 3710_Platformer/Assets/ScrollPickup.cs
+++ 3710_Platformer/Assets/ScrollPickup.cs
@@ -1,54 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HealthPickup : MonoBehaviour {
+public class ScrollPickup : MonoBehaviour {
 
-    public int HealAmount = 1;
     public int bounceHeight = 10;
 
 
     private Transform _controller;
+    private AnimationController2D _animator;
     private float bounceFrame = 0;
     private bool falling = false;
     private bool isPickup = true;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         _controller = gameObject.GetComponent<Transform>();
+        _animator = gameObject.GetComponent<AnimationController2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         animatePosition();
-	}
+        _animator.setAnimation("glow");
+    }
 
     // If player collides and isn't max health, heal'em up
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(isPickup && col.tag == "Player")
-        {
-            PlayerController thePlayer = col.gameObject.GetComponent<PlayerController>();
-            if(thePlayer.getCurrentHealth() < thePlayer.startingHealth)
-            {
-                thePlayer.addToHealth(HealAmount);
-                Destroy(this.gameObject);
-            }
-
-        }
+        
     }
 
     // animate a little bounce
     void animatePosition()
     {
         Vector3 old = _controller.position;
-        
-        if(falling){
+
+        if (falling)
+        {
             bounceFrame--;
-            _controller.position = new Vector3(old.x, old.y - 1 * Time.deltaTime/2, old.z);
+            _controller.position = new Vector3(old.x, old.y - 1 * Time.deltaTime / 4, old.z);
         }
-        else{
+        else
+        {
             bounceFrame++;
-            _controller.position = new Vector3(old.x, old.y + 1 * Time.deltaTime/2, old.z);
+            _controller.position = new Vector3(old.x, old.y + 1 * Time.deltaTime / 4, old.z);
         }
         if (bounceFrame > bounceHeight)
         {
